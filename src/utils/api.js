2f28c660a923487100123a6e639ab8adb6dba07c
Mark Lester Bolotaolo <mlab817@gmail.com>
2022-11-07 21:13:59 +0800

    added official endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.254.102:8000/api',
+  baseURL: 'https://api.pips.da.gov.ph/api',
   // headers: {
   //   common: {
   //     Accept: 'application/json',
